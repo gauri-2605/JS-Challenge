d9c3bc50e6664c264020999783f5bdd827b9f5fe
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-03 11:12:33 +0000

    datatype done

---
+++ Day_2/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //Treat all JS code as newer version 
+
+// alert(3+3) // we are using node js not browser.JS engine are hidden in browser
+
+console.log(3
+   +
+   8) //code readability shoulde be high
+
+let name="Gauri" //strind Datatype
+let age=18//number
+let isLoggedIn=false
+
+//number=2 to power 53
+//bigInt=for big integer
+//string => ""
+//boolean=>1,0,true/false
+//null=> it is standalone value
+//undefined =>when value not assigned
+//symbol =>for find uniqueness 
+
+//object
+
+console.log(typeof null);  //object
+console.log(typeof undefined);//undefined
+
