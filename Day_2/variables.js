950f5c50e2c94a3b1fc46a48a1c7508ce5c15b6d
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-03 10:24:35 +0000

    Add variables.js example

---
+++ Day_2/variables.js
@@ -0,0 +1,13 @@
+const accountId = 14435
+let accountEmail = "gauri@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 4   <-- remove or comment
+accountEmail = "gauri123.com"
+accountPassword = "gauri"
+accountCity = "Bengaluru"
+
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
