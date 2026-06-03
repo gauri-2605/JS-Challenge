ce7558398c9bf3b487147c72506f6ab5f9d4848c
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-03 11:46:39 +0000

    conversion discuss

---
+++ Day_2/conversionoperation.js
@@ -0,0 +1,37 @@
+let score=33
+
+//console.log(typeof score);
+//console.log(typeof(score)); //using method
+
+let valueInNumber=Number(score) //doing conversion strinf to number
+console.log(typeof valueInNumber);
+
+// let score=33abc,null,undefined,true
+
+// //console.log(typeof score);
+// //console.log(typeof(score)); //using method
+
+// let valueInNumber=Number(score) //doing conversion strinf to number
+// console.log(typeof valueInNumber);
+
+//"44" => 44
+//"23abc" =>NaN
+//true =>1
+//string =>NaN
+
+let bool="gauri"  //now we want to convert it 
+
+let newOne=Boolean(bool)
+console.log(newOne);
+
+//when 1 then in boolean it is true ,0 then false
+//"" =>false
+//"gauri"=>true5
+
+let somenumber=45
+let stringNumber=String(somenumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+
