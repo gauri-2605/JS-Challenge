5de22eafa08996c44d945e48b18f50252bb75d12
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-04 18:25:10 +0000

    comparision changes done

---
+++ Day_3/comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//Problem come when same datatype nor compare kr sakte
+
+// console.log("2">1);//here it automatically convert 2 into number
+// console.log("02">1);//notr:typescript not allow to compare 2 different datatypes
+
+// console.log(null>0);
+ console.log(null==0);
+// console.log(null>=0); // here problm where null is greater then values cnversion prblm,predictive rsult not comes
+
+//In JS null convert into 0
+//In JS comparision and equality operator are 2 method
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+// //=== strict check not only check value but datatypes also
+// console.log("2"===2);
+// console.log(2===2);
---
+++ Day_3/conversionoperation.js
@@ -0,0 +1,75 @@
+let score=33
+
+//console.log(typeof score);
+//console.log(typeof(score)); //using method
+
+// let valueInNumber=Number(score) //doing conversion strinf to number
+// console.log(typeof valueInNumber);
+
+// let score=33abc,null,undefined,true
+
+// //console.log(typeof score);
+// //console.log(typeof(score)); //using method
+
+// let valueInNumber=Number(score) //doing conversion strinf to number
+// console.log(typeof valueInNumber);
+
+//"44" => 44
+//"23abc" =>NaN
+//true =>1
+//string =>NaN
+
+let bool="gauri"  //now we want to convert it 
+
+// let newOne=Boolean(bool)
+// console.log(newOne);
+
+//when 1 then in boolean it is true ,0 then false
+//"" =>false
+//"gauri"=>true5
+
+ let somenumber=45
+// let stringNumber=String(somenumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+// ---------------------OPERATIONS-------------------------------------------//
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+let str1="Gauri"
+let str2=" Badgujar"
+let str3=str1+str2
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2);
+// console.log("1"+ 2 +2);//if string first then treat all as string but if simpple then first conversion 
+// console.log(1 +2 + "2");// not depend on this
+// console.log(1 +"2" + 2+2);
+// console.log(1 +2 + "2"+2);
+// console.log((1 +2) * 2 % 2); // for such type used paranthesis it is good 
+
+console.log(+true);//it is not good thing
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+1 //it is also not good practice
+
+let gameCounter=100   // in prefix value before increment postfix me after incremrent 
+//for more  extra info see js  mdn preand post see
+gameCounter++  
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
