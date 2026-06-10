4960140151df7e6414e2e88b2fdc0275fc58447a
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-10 18:48:05 +0000

    scope

--- Day_9/scope.js
+++ Day_9/scope.js
@@ -27,3 +27,4 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
+//imp question :scope in environemnt node and after inspect when see scope both different
