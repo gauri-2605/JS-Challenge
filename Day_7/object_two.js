c030fda92a8d720fc3a59be2c5e631abb6efd6a1
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-10 09:40:13 +0000

    json

--- Day_7/object_two.js
+++ Day_7/object_two.js
@@ -59,5 +59,33 @@ console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+//De -structure
+
+const course =
+{
+   course_name:"JS",
+   course_price:"999",
+   course_Instructor :"hitesh"
+}
+
+console.log(course.course_Instructor)
+
+//when we make code clean ,so again and again course.course_Instructor so use below
+
+const {course_Instructor} =course
+console.log(course_Instructor);
+
+ const {course_Instructor: instructor} =course //also gave another name ,here see how we destructure object
+// console.log(instructor);
+
+// const navbar = ({}) =>
+// { //when {} then this is destructure
+
+// }
+// navbar(company ="TCS")
 
 
+/* API  :APNA KAM DUSRO KO DENA
+API nothing but  values comes from backendand how you use this values
+in earlier Value comess  in xml this is very complicated ,nnow it is JSON
+/*
