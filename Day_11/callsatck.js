0b67cc2ca83f67ec78203d28039a3d4976b1ce9a
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-13 13:00:54 +0000

    call

--- Day_11/callsatck.js
+++ Day_11/callsatck.js
@@ -2,6 +2,27 @@
 
 Run in 2 Phase 
 
+First code gives to JS ,so  first thing is GLOBAL EC always created ,it refere to this 
+browser GEC is window.Run in thread ,JS is single threaded.
+
+1.Global EC
+2.Functional EC
+3.Eval EC  :Mongos,and all inludes
+
+Also Memory creation phase: Only for variables memory alloctaed
+Execution Phase:-,+ math operation here runs
 */
-console.log("HIII");
+
+
+let val1=12
+let val2=11
+
+function addNUm(n1,n2)
+{
+    let total =n1+n2;
+    return total
+}
+let result = addNUm(val1, val2)
+console.log(result);
+
 
