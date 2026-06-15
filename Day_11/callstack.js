8939a89ec22542de6325296e0cffdecb571a99de
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-15 13:35:31 +0000

    callstack

---
+++ Day_11/callstack.js
@@ -0,0 +1,34 @@
+/*   JS Execution Context  :How to run code file 
+
+Run in 2 Phase 
+
+First code gives to JS ,so  first thing is GLOBAL EC always created ,it refere to this 
+browser GEC is window.Run in thread ,JS is single threaded.
+
+1.Global EC
+2.Functional EC
+3.Eval EC  :Mongos,and all inludes
+
+Also Memory creation phase: Only for variables memory alloctaed
+Execution Phase: -,+ math operation here runs
+*/
+
+
+let val1=12
+let val2=11
+
+function addNUm(n1,n2)
+{
+    let total =n1+n2;
+    return total
+}
+let result = addNUm(val1, val2)
+let result =addNUm(10,7)
+console.log(result);
+
+/*Call Stack
+
+In first Always Global E there then functions comes ,works perform qnd thrn removes.
+
+
+
