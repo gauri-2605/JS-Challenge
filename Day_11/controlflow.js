c76ed979b07c81faa6d3b88b99ffb44edd613193
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-15 11:03:55 +0000

    IFELSE

---
+++ Day_11/controlflow.js
@@ -0,0 +1,50 @@
+/*  Always whole code not to run.Whole code run in conditional based
+*/
+
+const  isUserLoggedIn = true //here = it is operator assignment 
+const temp =41
+
+if (temp < 50)  //here conditon is true then it goes to inside and run ,if false then code not run
+{
+   console.log("Temp less than 50");
+   
+}
+else
+{
+   console.log("Temp greater than 50");
+   
+}
+console.log("Exeted"); // it is always executed it not related to if for that we used else keyword
+
+
+/*const score = 200
+if(score > 100)
+{
+    const power ="fly"  // var "fly"
+    console.log(`User  Power: ${power}`);
+}
+console.log(`User  Power: ${power}`); //problem here because power is not defined */
+
+ const balance =1000
+// if(balance >500) console.log("Test"),console.log("Test2");  //implicit scope only in 1 line but want more then used comma and continue log
+ 
+
+if(balance < 500)
+{
+   console.log("Less than 500");
+}
+else if(balance < 750)
+{
+   console.log("Less than 750");
+
+}
+else if(balance < 900)
+{
+   console.log("Less than 900");
+
+}
+else
+{
+   console.log("Less than 12000");
+   
+}
