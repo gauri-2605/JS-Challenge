d11e3056140e1f0a878175f55cf34ea3bbea292b
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-15 14:42:21 +0000

    switch

---
+++ Day_11/ControlFlow/switch.js
@@ -0,0 +1,43 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month =0
+
+switch (month) 
+{
+    case 1:
+        console.log("JANUARY");
+        break; //by default where key match then all after this executed.If break not use then all cases run except Default
+    case 2:
+        console.log("FEBRUARY");
+        break;
+    case 3:
+        console.log("MARCH");
+        break;
+    case 4:
+        console.log("APRIL");
+        break;
+    case 5:
+        console.log("MAY");
+        break;
+    case 6:
+        console.log("JUNE");
+        break;
+    case 7:
+        console.log("JULY");
+        break;
+    case 8:
+        console.log("AUGUST");
+        break;
+
+    default:
+
+        console.log("Default");
+        break;
+}
