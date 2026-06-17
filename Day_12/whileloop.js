c45606dfccaef4f4681497e4c9c3c61c78ea6fba
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-17 08:34:50 +0000

    whileloop

---
+++ Day_12/whileloop.js
@@ -0,0 +1,25 @@
+// let index =0
+// while(index <= 10)
+// {
+//     // console.log(`Value of Index is ${index}`);
+//     // index =index+2 
+// }
+
+let myArr=['abc','xyz','pqr']
+
+let arr=0
+// while(arr <myArr.length)
+// {
+//     console.log(`Value is ${myArr[arr]}`);
+//     arr=arr+1
+// }
+
+//do-while ,condition at last 
+
+let score=11
+
+do
+{
+    console.log(`Score is ${score}`);
+    score++
+}while (score <=10)
