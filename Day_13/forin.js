e6086eb6f443b002fe2107b255adf67b22acfe6d
gauri-2605 <gauribadgujar8@gmail.com>
2026-06-17 19:17:12 +0000

    loops done

--- Day_13/forin.js
+++ Day_13/forin.js
@@ -1,35 +1,35 @@
-const myObject={
-    js:"JAVASCRIPT",
-    cpp:"C++",
-    rb:"ruby",
-    swift:"swift by apple"
-}
+// const myObject={
+//     js:"JAVASCRIPT",
+//     cpp:"C++",
+//     rb:"ruby",
+//     swift:"swift by apple"
+// }
 
-for (const key in myObject) {
-//   console.log(key);
-}
+// for (const key in myObject) {
+// //   console.log(key);
+// }
 
-//for value,above only gives keys
+// //for value,above only gives keys
 
-for (const key in myObject) {
-//   console.log(myObject[key]);
-  console.log(`${key} shortcut is for ${myObject[key]}`);
+// for (const key in myObject) {
+// //   console.log(myObject[key]);
+//   console.log(`${key} shortcut is for ${myObject[key]}`);
   
-}
+// }
 
-const arr =["js","cpp","ruby","python"]
+// const arr =["js","cpp","ruby","python"]
 
-for (const key in arr) { //here when we say key then it gives index numbers.In array key start from 0 to continue
-                                 //but in object bydefault any keys we can write
-   console.log(arr[key] );
+// for (const key in arr) { //here when we say key then it gives index numbers.In array key start from 0 to continue
+//                                  //but in object bydefault any keys we can write
+//    console.log(arr[key] );
     
-}
+// }
 
-const map =new Map()   //for map ,for in is not iterable ,so not execute
-map.set('IN',"INDIA")
-map.set('USA',"United States of America")
-map.set('Fr',"France")
+// const map =new Map()   //for map ,for in is not iterable ,so not execute
+// map.set('IN',"INDIA")
+// map.set('USA',"United States of America")
+// map.set('Fr',"France")
 
-for (const key in map) { 
-    console.log(key);
-}
+// for (const key in map) { 
+//     console.log(key);
+// }
